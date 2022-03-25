d4c87b3c741daea94173b831f1a0167ef5361bdf
acrodrigo <alencarcosta3@gmail.com>
2022-03-24 22:17:16 -0300

    Aula 256 - CRUD Scaffolding

--- SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMVC/Models/ViewModels/ErrorViewModel.cs
@@ -1,7 +1,7 @@
 using System;
 
 // Atencao pois no projeto do curso eh namespace SalesWebMVC.Models.ViewModels
-namespace SalesWebMVC.Models.ViewModels {
+namespace SalesWebMVC.Models {
     public class ErrorViewModel {
         public string RequestId { get; set; }
 
